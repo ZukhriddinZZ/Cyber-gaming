78b1aa238d35df3006ece97015dc46624f73c4a9
Your Name <ziyodullayevzuhriddin7@gmail.com>
2023-05-10 16:43:29 +0500

    some parameters have been changed

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// let mNav = document.querySelector(".mobile_nav");
-// let mbtn = document.querySelector(".menu_button");
-
-// mbtn.addEventListener("click", function () {
-//   cosole.log("Hello World");
-
-//   mobileNav.classList.toggle("showNav");
-
-//   if (mNav.classList.contains("clickshow")) {
-//     mNav.classList.remove("clickshow");
-//   } else {
-//     mNav.classList.add("clickshow");
-//   }
-// });
-
 let menuBtn = document.querySelector(".menu_button");
 let mobileNav = document.querySelector(".mobile_nav");
 
@@ -20,9 +5,9 @@ menuBtn.addEventListener("click", function () {
   console.log("Hello World");
   mobileNav.classList.toggle("showNav");
 
-  if (mobileNav.classList.contains(".clickshow")) {
-    mobileNav.classList.remove(".clickshow");
-  } else {
-    mobileNav.classList.add(".clickshow");
-  }
+  // if (mobileNav.classList.contains(".clickshow")) {
+  //   mobileNav.classList.remove(".clickshow");
+  // } else {
+  //   mobileNav.classList.add(".clickshow");
+  // }
 });
